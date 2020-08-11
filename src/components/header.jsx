f63675531998c16728e99f6d4a383d08c7094e08
GAUTAMSAHARAN <gsooouuu@gmail.com>
2020-08-11 17:04:43 +0530

    basic header layout ready

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,64 @@
 import React, { Component } from 'react';
+import cart from '../images/cart.png';
+import user from '../images/user.png';
 import '../styles/pages/header.scss';
 
+const items = [
+    {
+        title: 'HOME',
+    },
+    {
+        title: 'SHOP ALL',
+    },
+    {
+        title: 'FOR FACE',
+    },
+    {
+        title: 'FOR HAIR',
+    },
+    {
+        title: 'ABOUT',
+    },
+    {
+        title: 'CONTACT',
+    },
+    {
+        title: 'FAQ',
+    }
+]
 
 
 class Header extends Component {
+
+    constructor(props){
+        super(props);
+        this.setState = {
+        }
+        this.listItems = this.listItems.bind(this);
+    }
+
+    listItems(){
+        const list = items.map((item) => 
+        <div className='hvr-overline-from-center'>{item.title}</div>
+        )
+        return(
+            list
+        )
+    } 
+
     render(){
         return(
           <>
-          
+            <div className="header">
+                <div className="logo">LOGO</div>
+                 {this.listItems()}
+                <div className='logo'>
+                    <img src={cart} />
+                </div>
+                <div className='logo'>
+                    <img  src={user} />
+                </div>
+            </div>
           </>
         )
     }
