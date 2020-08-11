9d054c5f502475a0a7a48a9dcff49f0ef3781888
GAUTAMSAHARAN <gsooouuu@gmail.com>
2020-08-11 16:05:45 +0530

    project base done

---
+++ src/components/header.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../styles/pages/header.scss';
+
+
+
+class Header extends Component {
+    render(){
+        return(
+          <>
+          
+          </>
+        )
+    }
+}
+
+export default Header;
