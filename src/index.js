63dd2da6595e86eef53889eb50c27dfc0d3a843a
K-Kumar-01 <59891164+K-Kumar-01@users.noreply.github.com>
2020-08-16 01:09:24 +0530

    added checkout

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import PersistentDrawerLeft from './components/drawer';
 import ProductList from './components/products';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ProductDetail from './components/productDetail';
+import Checkout from './components/Checkout/Checkout';
 ReactDOM.render(
-  <React.StrictMode>
-    <PersistentDrawerLeft />
-    <ProductList />
-    <ProductDetail/>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<PersistentDrawerLeft />
+		<ProductList />
+		<ProductDetail />
+		<Checkout />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
-
-
