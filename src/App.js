2f59d0075f228e40b01f974c9531527f7aa15275
GAUTAMSAHARAN <gsooouuu@gmail.com>
2020-08-18 14:39:18 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter} from 'react-router-dom';
+import ROUTES, { RenderRoutes } from "./config/routes";
+import PersistentDrawerLeft from './components/drawer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+    <PersistentDrawerLeft />
+    <RenderRoutes routes={ROUTES} />
+    </BrowserRouter>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PersistentDrawerLeft from './components/drawer';
-import ProductList from './components/products';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ProductDetail from './components/productDetail';
-import Checkout from './components/Checkout/Checkout';
+import App from './App';
+
 ReactDOM.render(
 	<React.StrictMode>
-		<PersistentDrawerLeft />
-		<ProductList />
-		<ProductDetail />
-		<Checkout />
+    <App />
 	</React.StrictMode>,
 	document.getElementById('root')
 );
